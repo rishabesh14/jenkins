8cd57d1967e048ae94e24814725a479e7d918664
rishabesh14 <rishabesh56@gmail.com>
2026-08-26 18:25:30 +0530

    version 1

---
+++ numberchecker.py
@@ -0,0 +1,10 @@
+def check_even_odd(num):
+    if num % 2 == 0:
+        print("The number is Even")
+    else:
+        print("The number is Odd")
+
+
+num = int(input("Enter a number: "))
+
+check_even_odd(num)
